a945beb6b649a824ef4db7f85c1e79d12b798d92
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-09-14 23:31:01 -0400

    create directory

--- chef/cookbooks/common/recipes/setup.rb
+++ chef/cookbooks/common/recipes/setup.rb
@@ -1,1 +1,9 @@
 package ['build-essential', 'git']
+
+directory node['deploy']['app_path'] do
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0755'
+  recursive recursive_create
+  action :create
+end
