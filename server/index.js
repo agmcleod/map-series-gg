57477fd907e232943a7b4cdf97710c237eb56296
Aaron McLeod <aaron.g.mcleod@gmail.com>
2018-03-02 00:49:32 -0500

    working on docker setup for app server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ router.use('/login', (req, res) => {
   res.json({ ok: true })
 })
 
+// notes for setting up actual endpoints here
+
+/**
+ * http://docs.couchdb.org/en/latest/intro/security.html
+ * curl -X PUT http://localhost:5984/_users/org.couchdb.user:jan \
+     -H "Accept: application/json" \
+     -H "Content-Type: application/json" \
+     -d '{"name": "jan", "password": "apple", "roles": [], "type": "user"}'
+ */
+
 app.use('*', require('body-parser').json())
 app.use('/', router)
 app.listen(8200, () => {
